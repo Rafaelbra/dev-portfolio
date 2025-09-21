0ff3f42fce1bd97a6f631e18a1b12afb15e1bc27
Rafael Braga <rafaelbra149@gmail.com>
2025-09-21 10:29:30 -0400

    Introducing Phone mobile navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { cn } from "../lib/utils";
+import { X, Menu } from "lucide-react";
 
 const navItems = [
     {name: "Home", href: "#hero"},
@@ -11,6 +12,7 @@ const navItems = [
 
 export default function Navbar() {
     const [isScrolled, setIsScrolled] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     useEffect(() => {
         const handleScroll = () => {
@@ -40,6 +42,35 @@ export default function Navbar() {
                                 {item.name}
                             </a>
                         ))}
+                    </div>
+
+                    {/* Mobile navbar */}
+                    <button 
+                     onClick={() => setIsMenuOpen((prev) => !prev)} 
+                     className="md:hidden p-2 text-foreground z-50" 
+                     aria-label={isMenuOpen ? "Close Menu" : "Open Menu"}
+                    >
+                        {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
+                    </button>
+
+                    <div className={cn("fixed inset-0 bg-background/95 backdrop-blur-md z-40 flex flex-col items-center justify-center",
+                    "transition-all duration-300 md:hidden",
+                    isMenuOpen 
+                     ? "opacity-100 pointer-events-auto" 
+                     : "opacity-0 pointer-events-none"
+                    )}>
+                        <div className="flex flex-col space-y-8 text-xl">
+                            {navItems.map((item, key) => (
+                                <a 
+                                key={key} 
+                                href={item.href} 
+                                className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                                onClick={() => setIsMenuOpen(false)}
+                                >
+                                    {item.name}
+                                </a>
+                            ))}
+                        </div>
                     </div>
                 </div>
         </nav>
