b3331e24542853b8128620ba2f507afb5b06576a
Rafael Braga <rafaelbra149@gmail.com>
2025-09-21 17:59:20 -0400

    Adding styles and animations to Hero Section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,12 +5,24 @@ export default function HeroScetion() {
         <section id="hero" className="relative min-h-screen flex felx-col items-center justify-center px-4">
             <div className="container max-w-4xl mx-auto text-center z-10">
                 <div className="space-y-6">
-                    <h1>
-                        <span> Hi, I'm</span>
-                        <span> Rafael</span>
-                        <span> Braga</span>
+                    <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
+                        <span className="opacity-0 animate-fade-in"> Hi, I'm</span>
+                        <span className="text-primary opacity-0 animate-fade-in-delay-1"> Rafael</span>
+                        <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2"> Braga</span>
                     </h1>
+                    <p className="text-lg md:text-xl text-muted foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">
+                        I craft stellar web experiences. As a front-end developer, I build smooth, responsive, and user-friendly interfaces, bringing ideas to life with clean design and solid functionality.
+                    </p>
+
+                    <div className="pt-4 opacity-0 animate-fade-in-delay-4">
+                        <a href="#projects" className="cosmic-button">
+                            View My Work
+                        </a>
+                    </div>
                 </div>
+            </div>
+            <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce">
+                <span>Scroll</span>
             </div>
         </section>
     )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -31,7 +31,7 @@ export default function Navbar() {
                 <div className="container flex items-center justify-between">
                     <a className="text-xl font-bold text-primary flex items-center" href="#hero">
                         <span className="relative z-10">
-                            <span className="text-glow text-foreground"> Rafael</span> Portfolio
+                            <span className="text-glow text-foreground"> Rafael's</span> Portfolio
                         </span>
                     </a>
 
